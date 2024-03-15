79e74532f39929a1f8cb5b6b4859ad255f10a090
Benjamin <benjamin.morales3@mail.udp.cl>
2024-03-15 10:35:07 -0300

    fixes

--- src/pages/principal/Principal.jsx
+++ src/pages/principal/Principal.jsx
@@ -8,14 +8,14 @@ const Principal = () => {
     const [state, setState] = useState("login");
     //pagina que se ve al no estar logeado, get started
     return (
-        <div className="relative flex flex-col bg-primary-custom">
-            <div className="flex flex-col lg:flex-row pt-2 px-6">
+        <div style={{minHeight: "100vh"}} className="relative flex flex-col bg-primary-custom">
+            <div className="flex flex-col lg:flex-row pt-2 px-6 lg:p-36">
                 <Animation duration={0.5}>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col lg:w-2/3 xl:w-1/2">
                         <h1 className="text-5xl font-bold text-white">Bienvenido a Salud Mental</h1>
                         <p className="font-semibold underline text-white text-lg text-right">Tu espacio seguro.</p>
                         <p className="text-white mt-2">Un espacio libre y seguro para compartir y expresar tus emociones. Comparte con la comunidad y encuentra lo que necesitas.</p>
-                        <button type="button" className="bg-secondary-custom px-4 py-2 rounded text-white font-bold my-6">Ver presentación de la app</button>
+                        <button type="button" className="w-1/2 bg-secondary-custom px-4 py-2 rounded text-white font-bold my-6">Ver presentación de la app</button>
                     </div>
                 </Animation>
                 <div className="flex flex-col justify-center items-center">
@@ -27,6 +27,7 @@ const Principal = () => {
             </div>
 
             {/*animación de una wave*/}
+            <div className="md:hidden">
             <Wave
                 fill="#fff"
                 paused={false}
@@ -39,6 +40,7 @@ const Principal = () => {
                     }
                 }
             />
+            </div>
         </div>
     )
 }
