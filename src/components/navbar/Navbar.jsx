2fccff945c5f847bc1f60bc183b00c8297b8f318
Renato <renato.contreras@mail.udp.cl>
2024-03-17 23:13:47 -0300

    v5

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,35 +2,24 @@ import { useState } from "react";
 
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
+    
     return (
-        <nav className=" bg-third-custom fixed w-full z-20 top-0 start-0 border-b">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://e7.pngegg.com/pngimages/228/99/png-clipart-mental-health-counselor-therapy-clinic-counseling-logo-psychology.png" className="h-8" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">S-Mental</span>
-                </a>
-                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button 
-                        onClick={() => setMenu(!menu)} 
-                        type="button" 
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
-                        aria-expanded={menu ? "true" : "false"}
-                    >
-                        <span className="sr-only">Menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-                        </svg>
-                    </button>
-                </div>
-                <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${menu ? 'block' : 'hidden'}`} id="navbar-sticky">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <a href="#" className="block py-2 px-3 text-white bg-primary-custom rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Inicio</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Informes</a>
-                        </li>
-                    </ul>
+        <nav className="bg-third-custom p-4 flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+                <span className="icon-[mdi--head-heart-outline] size-9 text-primary-custom "></span>
+                <span className="text-primary-custom font-semibold text-lg whitespace-nowrap">S-Mental</span>
+            </div>
+            
+            <div className="flex items-center justify-end space-x-4">
+                <button className="bg-gray-700 text-primary-custom px-1 py-1 rounded-md hover:bg-gray-600 flex items-center">
+                    <span className="icon-[solar--settings-linear] size-7"></span>
+                </button>
+
+                <div className="flex items-center space-x-2 md:flex">
+                    <img src="https://via.placeholder.com/40" alt="Foto de perfil" className="h-9 w-9 rounded-full border-2 border-gray-700" />
+                    <div className="text-primary-custom font-medium">
+                        <span className="hidden md:inline-block">Usuario</span>
+                    </div>
                 </div>
             </div>
         </nav>
