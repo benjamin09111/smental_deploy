06fa3d5b0c35fb1423eb17ef696c5b1832ff118a
Renato <renato.contreras@mail.udp.cl>
2024-03-19 00:46:19 -0300

    V10

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,33 +1,43 @@
-import {useState, useEffect} from 'react';
+// Main.js
+import { useState, useEffect } from 'react';
 import Footer from '../../components/footer/Footer';
 import Navbar from '../../components/navbar/Navbar';
 import Aside from "../../components/aside/Aside";
 import Content from "../main/content/Content";
+import NavbarHome from '../../components/nav_home/NavbarHome';
 
 const Main = () => {
-    //todo el contenido de la app
     const [state, setState] = useState("home");
     const [tipo, setTipo] = useState("psicologo");
+    const [searchMode, setSearchMode] = useState(false);
 
-    useEffect(()=> {
-        //llamada para saber el tipo, hay que crifrar
+    useEffect(() => {
+        // Llamada para saber el tipo, hay que cifrar
     }, [])
-    
+
+    const handleStateChange = (newState) => {
+        if (newState === "buscar") {
+            setSearchMode(true);
+        } else {
+            setSearchMode(false);
+        }
+        setState(newState);
+    }
+
     return (
-        <div style={{minHeight: "100vh"}} className='flex flex-col bg-gradient-secondary text-white'>
+        <div style={{ minHeight: "100vh" }} className='flex flex-col bg-gradient-secondary text-white'>
             <div>
-                <Navbar tipo={tipo} state={state} setState={setState} ></Navbar>
+                {searchMode ? <NavbarHome tipo={tipo} /> : <Navbar tipo={tipo} state={state} setState={handleStateChange} />}
             </div>
-            <main style={{paddingTop: "10vh"}} className='flex w-full'>
-                <Aside tipo={tipo} setState={setState}/>
+            <main style={{ paddingTop: "10vh" }} className='flex w-full'>
+                <Aside tipo={tipo} setState={handleStateChange} />
                 <Content tipo={tipo} setTipo={setTipo} state={state} />
             </main>
             <div className="lg:hidden">
-                <Footer tipo={tipo} setState={setState} />
+                <Footer tipo={tipo} setState={handleStateChange} />
             </div>
         </div>
-        
     )
 }
 
-export default Main
+export default Main;
