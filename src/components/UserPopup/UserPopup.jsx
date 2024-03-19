f82300c0f211bdb9e5e2241408028898e902a359
Renato <renato.contreras@mail.udp.cl>
2024-03-19 01:52:38 -0300

    V13

---
+++ src/components/UserPopup/UserPopup.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const UserPopup = () => {
+  return (
+    <div className="mx-auto w-60">
+      <div className="bg-white rounded overflow-hidden shadow-lg">
+        <div className="text-center p-6 bg-third-custom">
+          <svg
+            aria-hidden="true"
+            role="img"
+            className="h-24 w-24 text-white rounded-full mx-auto"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 256 256"
+          >
+            <path
+              fill="currentColor"
+              d="M172 120a44 44 0 1 1-44-44a44 44 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.2 104.2 0 0 1 104 104Zm-16 0a88 88 0 1 0-153.8 58.4a81.3 81.3 0 0 1 24.5-23a59.7 59.7 0 0 0 82.6 0a81.3 81.3 0 0 1 24.5 23A87.6 87.6 0 0 0 216 128Z"
+            ></path>
+          </svg>
+          <p className="pt-2 text-lg font-semibold text-gray-50">John Doe</p>
+          <p className="text-sm text-gray-100">John@Doe.com</p>
+          <div className="mt-5">
+            <button className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100 hover:bg-gray-800">
+              Ver perfil
+            </button>
+          </div>
+        </div>
+
+        <div className="border-b">
+          <button className="px-4 py-2 hover:bg-gray-100 flex w-full items-center">
+            <span className="icon-[solar--settings-linear] size-5 text-gray-800"></span>
+            <span className="pl-3 text-sm font-medium text-gray-800 leading-none">
+              Ajustes
+            </span>
+          </button>
+        </div>
+
+        <div className="">
+          <button href="#" className="w-full px-4 py-2 pb-4 hover:bg-gray-100 flex">
+            <p className="text-sm font-medium text-gray-800 leading-none">Cerrar sesión</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserPopup;
--- src/components/nav_home/NavbarHome.jsx
+++ src/components/nav_home/NavbarHome.jsx
@@ -1,8 +1,15 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import UserPopup from '../UserPopup/UserPopup';
 
 const NavbarHome = ({ tipo }) => {
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true);
+    const [showUserPopup, setShowUserPopup] = useState(false);
+    const popupRef = useRef(null);
+
+    const toggleUserPopup = () => {
+        setShowUserPopup(!showUserPopup);
+    };
 
     useEffect(() => {
         const handleScroll = () => {
@@ -17,25 +24,43 @@ const NavbarHome = ({ tipo }) => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, [prevScrollPos]);
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (popupRef.current && !popupRef.current.contains(event.target)) {
+                setShowUserPopup(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <nav style={{ height: "10vh", zIndex: "999999", transition: "top 0.3s" }} className={`bg-gray-900 p-4 flex items-center justify-between fixed ${visible ? "top-0" : "-top-[92px]"} lg:top-0 left-0 w-full`}>
+        <nav style={{ height: "10vh", zIndex: "999999", transition: "top 0.3s" }} className={`bg-third-custom p-4 flex items-center justify-between fixed ${visible ? "top-0" : "-top-[92px]"} lg:top-0 left-0 w-full`}>
             <div className="flex items-center space-x-2">
                 <span className="icon-[mdi--head-heart-outline] size-9 text-primary-custom "></span>
                 <span className="text-primary-custom font-semibold text-lg whitespace-nowrap">S-Mental</span>
                 <div className="text-2xl text-gradient">{tipo === "psicologo" ? "Perfil: PSICÓLOGÍA" : (tipo === "admin" ? "Perfil: ADMIN" : "")}</div>
             </div>
             
-            <div className="flex items-center justify-end space-x-4">
-
-            <input type="text" placeholder="Buscar..." className="min-h-9 w-1/3 px-2 py-1 rounded bg-gray-700 text-primary-custom focus:outline-none" />
-
-                <div className="flex items-center space-x-2 md:flex">
+            {/* Contenedor flex para alinear el botón de búsqueda y la foto de usuario */}
+            <div className="flex items-center space-x-2">
+                <input type="text" placeholder="Buscar..." className="min-h-9 flex-grow px-2 py-1 rounded bg-gray-700 text-primary-custom focus:outline-none" />
+                <button onClick={toggleUserPopup} className="focus:outline-none flex items-center">
                     <img src="https://via.placeholder.com/40" alt="Foto de perfil" className="h-9 w-9 rounded-full border-2 border-gray-700" />
                     <div className="text-primary-custom font-medium">
                         <span className="hidden lg:inline-block">Usuario</span>
                     </div>
-                </div>
+                </button>
             </div>
+
+            {showUserPopup && (
+                <div ref={popupRef} className="absolute top-0 right-0 rounded-lg shadow-lg p-4">
+                    <UserPopup />
+                </div>
+            )}
         </nav>
     );
 }
