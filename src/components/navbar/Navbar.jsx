17ba0b6617efedb739a71557e8d2ae216d78c17d
Benjamin Morales <benjamin.morales3@mail.udp.cl>
2024-07-03 15:51:24 -0400

    fit: logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -41,15 +41,22 @@ const Navbar = ({ tipo, setState }) => {
     }, []);
 
     return (
-        <nav style={{ height: "8vh", zIndex: "999999", transition: "top 0.3s" }} className={`bg-secondary-custom px-4 flex items-center justify-between fixed ${visible ? "top-0" : "-top-[25rem]"} lg:top-0 left-0 w-full`}>
+        <nav style={{ height: "8vh", zIndex: "999999", transition: "top 0.3s" }} className={`bg-secondary-custom px-4 flex items-center text-lg lg:text-xl py-4 justify-around fixed ${visible ? "top-0" : "-top-[25rem]"} lg:top-0 left-0 w-full`}>
             <div className="flex items-center space-x-2">
-                <span className="icon-[mdi--head-heart-outline] size-12 text-primary-custom "></span>
-                <span className="text-primary-custom font-semibold text-2xl whitespace-nowrap">S-Mental</span>
+                <span className="icon-[mdi--head-heart-outline] size-10 text-primary-custom "></span>
+                <span className="text-primary-custom font-semibold text-xl whitespace-nowrap">S-Mental</span>
             </div>
             
             <div className='font-semibold text-xl text-primary-custom/90'>
                 {localStorage.getItem("nombre")}
             </div>
+
+            <button className='underline text-lg' onClick={()=> {
+                localStorage.removeItem("token");
+                localStorage.removeItem("id");
+                localStorage.removeItem("nombre");
+                window.location.href = "/";
+            }}>Salir</button>
         </nav>
     );
 }
